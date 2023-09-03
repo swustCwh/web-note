083f56a0eaa24e6fc0337d13b0ac99c58a5b5b60
cwh <xxx@xxx.com>
2023-09-03 21:56:48 +0800

    网络

--- 网络/Axios/Axios.js
+++ 网络/Axios/Axios.js
@@ -1,1 +1,16 @@
+/**
+ * @link Axios官方文档：https://www.axios-http.cn/
+ */
+
 const axios = require('axios')
+
+// axios.get('http://127.0.0.1:8004/request').then(res => {
+//     console.log(res.data)
+// })
+
+axios({
+    method: 'get',
+    url: 'http://127.0.0.1:8004/request'
+}).then(res=>{
+    console.log(res.data)
+})
---
+++ 网络/服务器/PublicServer.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.all('/server',(req,res)=>{
+    res.send({
+        name:'cwh',
+        age:20
+    })
+}) 
+
+app.all('/request',(req,res)=>{
+    res.send({
+        status:201,
+        value:'成功'
+    })
+})
+
+app.listen(8004,() => {
+    console.log('http://127.0.0.1:8004')
+})
