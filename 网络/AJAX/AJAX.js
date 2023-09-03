2fe809505986b6863c466546fb6e9f8f4db16f03
cwh <xxx@xxx.com>
2023-09-03 12:23:21 +0800

    网络

--- 网络/AJAX/AJAX.js
+++ 网络/AJAX/AJAX.js
@@ -1,19 +1,37 @@
-
-/**
- * 构造函数接收两个参数
- *      url：网络地址、async：是否开启异步
- */
-const url = 'http://127.0.0.1'
-const async = true
-const XHR = new XMLHttpRequest(url,async)
+const XHR = new XMLHttpRequest()
 
 XHR.onreadystatechange = function() {
+    /**
+     * readyState：表示请求/响应的阶段
+     *  0：未初始化（未调用 open 方法）
+     *  1：已打开（调用 open 方法）
+     *  2：已发送（调用 send 方法）
+     *  3：接受部分响应中
+     *  4：响应完成
+     */
     if(this.readyState === 4) {
-        // 表示请求成功
-        console.log(this.responseText)
+        /**
+         * status：HTTP状态码
+         *  1xx：请求成功但是要继续请求
+         *  2xx：响应成功
+         *  3xx：重定向
+         *  4xx：一般是请求报文错误
+         *  5xx：一般是服务器错误
+         */
+        if(XHR.status >= 200 && XHR.status < 300) {
+            /**
+             * responseText：响应体
+             * responseXML：响应内容的类型为 "test/xml" 或 "application/xml"
+             */
+        }
     }
 }
 
-XHR.open('GET')
+const options = {
+    method:'GET', // 请求方式
+    url:'http://127.0.0.1', // 请求地址
+    async:true // 是否开启异步请求
+}
+XHR.open(...options) 
 // 发送请求
 XHR.send()
