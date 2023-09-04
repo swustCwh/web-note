05147772f314df5f5fc1251c1eff09f2409dd0ad
cwh <xxx@xxx.com>
2023-09-04 13:32:18 +0800

    网络

--- 网络/Axios/Axios.js
+++ 网络/Axios/Axios.js
@@ -20,19 +20,53 @@ const instance = axios.create({
     baseURL:'http://127.0.0.1:8004',
     timeout:1000,
     headers:{
-        'user':'cwh'
+        'user':'cwh',
     }
 })
 
-instance.get('/request',{
 
-}).then(res => {
+/**
+ * post 请求
+ *  设置 content-type
+ *      1、application/x-www-form-urlencoded：表单数据
+ *      2、multipart/form-data：适用文件上传
+ *      3、application/json：JSON数据
+ * 
+ */
+// 设置请求体
+const body = {
+    user:'cwh',
+    age:20
+}
+const config  = {
+    headers:{
+        'content-type':'application/x-www-form-urlencoded'
+    }
+}
+instance.post('/server',body,config)
+.then(res => {
     console.log(res.data)
 })
 
+
+/**
+ * 通用请求格式
+ */
 instance({
     method: 'get',
-    url: '/request'
+    // 路径参数
+    url: '/request/params/name',
+    // 请求体参数
+    data:{
+        name:'name'
+    },
+    // 查询字符串参数
+    params:{
+        data:'params'
+    },
+    headers:{
+        data:'header'
+    }
 }).then(res=>{
     console.log(res.data)
 })
--- 网络/服务器/PublicServer.js
+++ 网络/服务器/PublicServer.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.all('/server',(req,res)=>{
+// 使用中间件解析post的请求体内容
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+app.post('/server',(req,res)=>{
+    console.log('params',req.params)
+    console.log('query',req.query)
+    console.log('headers',req.headers)
+    console.log('body',req.body)
     res.send({
         name:'cwh',
         age:20
@@ -13,6 +22,10 @@ app.get('/request',(req,res)=>{
     // req.query
     // req.body
     // req.params
+    console.log('params',req.params)
+    console.log('query',req.query)
+    console.log('headers',req.headers)
+    console.log('body',req.body)
     res.send({
         status:201,
         value:'成功'
